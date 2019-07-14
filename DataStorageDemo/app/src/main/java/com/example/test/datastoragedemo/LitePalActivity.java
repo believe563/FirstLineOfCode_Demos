6b2eacc27df7f6b1b748b6120c4750c15a329f53
believe563 <missjhfeng@qq.com>
2019-07-14 12:45:41 +0800

    Add LitePal to Project

---
+++ DataStorageDemo/app/src/main/java/com/example/test/datastoragedemo/LitePalActivity.java
@@ -0,0 +1,16 @@
+package com.example.test.datastoragedemo.utils;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.test.datastoragedemo.R;
+
+public class LitePalActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lite_pal);
+    }
+}
